336d3af5a228706d5e5c086e272e6f018a06cf0e
aoki <aoki@cubit.jp>
2017-04-11 19:39:00 +0900

    chenge input type=range => type=number

--- src/js/container/Form.jsx
+++ src/js/container/Form.jsx
@@ -5,7 +5,7 @@ import { ReactDOM } from 'react-dom'
 import { connect } from 'react-redux'
 //component
 //style
-import styles from '../../css/components/font_layer.css'
+import styles from '../../css/components/form.css'
 
 export default class Form extends React.Component {
 
@@ -21,39 +21,35 @@ export default class Form extends React.Component {
 		console.log(styles);
 
 		return (
-			<div>
+			<div className={styles.form}>
 				<dl>
 					<dt>font−size</dt>
 					<dd>
-						<span>{fontSize}px</span>
 						<input
-							type="range"
+							type="number"
 							name="fontSize"
-							min="10"
-							max="50"
-							value={fontSize}
+							defaultValue={fontSize}
 							onChange={(e) => changeFontSizeValue(e.target.value)}
 						/>
+						<span>px</span>
 					</dd>
 				</dl>
 				<dl>
 					<dt>line-height</dt>
 					<dd>
-						<span>{lineHeight}px</span>
 						<input
-							type="range"
+							type="number"
 							name="lineHeight"
-							min="1"
-							max="72"
-							value={lineHeight}
+							defaultValue={lineHeight}
 							onChange={(e) => changeLineHeightValue(e.target.value)}
 						/>
+						<span>px</span>
 					</dd>
 				</dl>
 				<dl>
 					<dt>text</dt>
 					<dd>
-						<textarea value='aaaaaaa' />
+						<textarea defaultValue='aaaaaaa' />
 					</dd>
 				</dl>
 			</div>
